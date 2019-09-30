16f446b91b9f54adfe70cd9a9c2dd785f7b817d6
Luiz Lima <edneto.eternamente@gmail.com>
2019-09-30 07:08:00 -0300

    example: do-while and while

---
+++ Aula13/ambiente.js
@@ -0,0 +1,19 @@
+/**** WHILE ESTRUTURA DE REPETIÇÃO COM 
+ * TESTE LÓGICO NO INICIO  */
+var c = 1
+do {
+    console.log('Contando: ' + c)
+    c++
+} while (c <= 8)
+
+
+
+
+/**** WHILE ESTRUTURA DE REPETIÇÃO COM
+ * TESTE LÓGICO NO INICIO  *//*
+var c = 1
+while(c <= 10){
+    console.log('Contando: ' + c)
+    c++
+}
+*/
